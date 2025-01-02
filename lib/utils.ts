7e3d60e6e5104db205888ddbc301ef10b53e6a4a
SilverCrow <123189414+SilverCrow0930@users.noreply.github.com>
2025-01-02 16:57:46 -0500

    Refactor authentication pages to use AuthForm component and add custom input handling

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -10,8 +11,8 @@ export function cn(...inputs: ClassValue[]) {
 // FORMAT DATE TIME
 export const formatDateTime = (dateString: Date) => {
   const dateTimeOptions: Intl.DateTimeFormatOptions = {
-    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
-    month: "short", // abbreviated month name (e.g., 'Oct')
+    weekday: "short", // abbreviated weekday e.g., 'Mon')
+    month: "short", // abbreviated month e.g., 'Oct')
     day: "numeric", // numeric day of the month (e.g., '25')
     hour: "numeric", // numeric hour (e.g., '8')
     minute: "numeric", // numeric minute (e.g., '30')
@@ -19,14 +20,14 @@ export const formatDateTime = (dateString: Date) => {
   };
 
   const dateDayOptions: Intl.DateTimeFormatOptions = {
-    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    weekday: "short", // abbreviated weekday e.g., 'Mon')
     year: "numeric", // numeric year (e.g., '2023')
-    month: "2-digit", // abbreviated month name (e.g., 'Oct')
+    month: "2-digit", // abbreviated month e.g., 'Oct')
     day: "2-digit", // numeric day of the month (e.g., '25')
   };
 
   const dateOptions: Intl.DateTimeFormatOptions = {
-    month: "short", // abbreviated month name (e.g., 'Oct')
+    month: "short", // abbreviated month e.g., 'Oct')
     year: "numeric", // numeric year (e.g., '2023')
     day: "numeric", // numeric day of the month (e.g., '25')
   };
@@ -193,3 +194,18 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+
+export const authFormSchema = (type:string) => z.object({
+   // sign up
+    firstName: type === 'sign-in' ? z.string().optional() : z.string().min(2),
+    lastName: type === 'sign-in' ? z.string().optional() : z.string().min(2),
+    address1: type === 'sign-in' ? z.string().optional() : z.string().min(5),
+    state: type === 'sign-in' ? z.string().optional() : z.string().min(2),
+    postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(10),
+    dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+    // both
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
