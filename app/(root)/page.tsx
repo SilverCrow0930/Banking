3186ad07d9670f9c2a42f7852b0a130f9c0b52b2
SilverCrow <123189414+SilverCrow0930@users.noreply.github.com>
2025-01-01 21:27:09 -0500

    Add new components for banking features and update types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import RightSidebar from "@/components/RightSidebar";
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
+  const loggedIn = { firstName:'Yinfan', lastName:'Mi', email:'heloyinfan@gmail.com'};
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:123.50},{currentBalance:158.92}]}
+      />
+    </section>
   )
 }
 
